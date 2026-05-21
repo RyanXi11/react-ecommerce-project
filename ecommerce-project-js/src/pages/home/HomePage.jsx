3923023ca3b9c4980f0e5875d89cbbb5671c6212
RyanXi11 <ryanxi57@gmail.com>
2026-05-21 18:30:32 -0400

    Convert ecommerce project to TypeScript

--- ecommerce-project-js/src/pages/home/HomePage.jsx
+++ ecommerce-project-js/src/pages/home/HomePage.jsx
@@ -7,20 +7,20 @@ import './HomePage.css';
 export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    const getHomeData = async () => {
+  useEffect(() =>{
+    const getHomeData = async () =>{
       const response = await axios.get('/api/products');
       setProducts(response.data);
     };
 
     getHomeData();
   }, []);
-
+  
   return (
     <>
       <title>Ecommerce Project</title>
 
-      <Header cart={cart} />
+      <Header cart={cart}/>
 
       <div className="home-page">
         <ProductsGrid products={products} loadCart={loadCart} />
